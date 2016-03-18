9925e5dc31606d460453212d572281906111f513
Vitalii Galkin <galkin@steelkiwi.com>
2016-03-18 21:24:30 +0200

    Add hide of add button if user already added to all the groups

--- client/filters.js
+++ client/filters.js
@@ -1,12 +1,20 @@
 angular.module('pizzaDayApp').filter('displayName', function () {
-  return function (user) {
-    if (!user)
-      return;
-    if (user.profile && user.profile.name)
-      return user.profile.name;
-    else if (user.emails)
-      return user.emails[0].address;
-    else
-      return user;
-  }
+    return function (user) {
+        if (!user)
+            return;
+        if (user.profile && user.profile.name)
+            return user.profile.name;
+        else if (user.emails)
+            return user.emails[0].address;
+        else
+            return user;
+    }
+});
+
+angular.module('pizzaDayApp').filter('showAddToGroup', function (owner_groups_ids) {
+    return function (user) {
+        console.log(user._id);
+        console.log(owner_groups_ids);
+        return true;
+    }
 });
--- client/users/controllers/users.ng.js
+++ client/users/controllers/users.ng.js
@@ -1,9 +1,26 @@
 angular.module('pizzaDayApp')
-    .controller('UserListController', ['$scope', '$meteor', '$modal',
-        function ($scope, $meteor, $modal) {
+    .controller('UserListController', ['$scope', '$meteor', '$modal', '$rootScope',
+        function ($scope, $meteor, $modal, $rootScope) {
             var owner_groups = $meteor.collection(Groups).subscribe('owner_groups');
             $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 
+            $scope.ifCouldBeAdded = function (user) {
+                return function () {   // inner function
+                    if (user._id == $rootScope.currentUser._id) {
+                        return false
+                    }
+                    if (!user.groups) {
+                        return true;
+                    }
+                    // here because if will be after owner_groups will be empty array
+                    // maybe there will be problems with "magic" reactivity
+                    let owner_groups_ids = [];
+                    for (let group of owner_groups) {
+                        owner_groups_ids.push(group._id);
+                    }
+                    return _.intersection(user.groups, owner_groups_ids).length != owner_groups_ids.length;
+                }();
+            };
             $scope.open = function (_user) {
 
                 var modalInstance = $modal.open({
@@ -29,7 +46,7 @@ angular.module('pizzaDayApp')
 
         $scope.owner_groups = owner_groups
             .filter(function (el) {
-                if (!user.groups){
+                if (!user.groups) {
                     return true;
                 }
                 return user.groups.indexOf(el._id) < 0;
