0b5ae40ecc2137711305fa208975ce1471367414
Vitalii Galkin <galkin@steelkiwi.com>
2016-03-21 08:47:49 +0200

    Add group resolve in detail

--- client/groups/group-detail/controllers.js
+++ client/groups/group-detail/controllers.js
@@ -1,11 +1,12 @@
 angular.module('pizzaDayApp')
-    .controller('GroupDetailController', ['$scope', '$stateParams', '$meteor', 'groupListFactory',
-            function ($scope, $stateParams, $meteor, groupListFactory) {
+    .controller('GroupDetailController', ['$scope', '$stateParams', '$meteor',
+            function ($scope, $stateParams, $meteor) {
 
                 var events = $meteor.collection(Events).subscribe('events');
                 //TODO add group validation
                 var group_id = $stateParams.id;
-                $scope.group = $meteor.object(Groups, group_id);
+                console.log(group_id);
+                $scope.group = $meteor.object(Groups, group_id).subscribe('groups');
                 //TODO rewrite with fetch one item from db
                 $scope.event = events[0];
                 $scope.users = $meteor.collection(Meteor.users, false).subscribe('group_users');
