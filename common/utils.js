470970df2af03da77be88d269fb5a7cab3b3f541
Vitalii Galkin <galkin@steelkiwi.com>
2016-03-31 00:26:38 +0300

    Fix email for oauth users and possible fix for saving file

--- common/utils.js
+++ common/utils.js
@@ -41,8 +41,8 @@ Utils = {
     contactEmail: (user) => {
         if (user.emails && user.emails.length)
             return user.emails[0].address;
-        if (user.services && user.services.facebook && user.services.facebook.email)
-            return user.services.facebook.email;
+        if (user.services && user.services.google && user.services.google.email)
+            return user.services.google.email;
         return null;
 
     }
